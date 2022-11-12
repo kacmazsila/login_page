e35447fc1ba3b490ef535e5334e908c964d7385c
Sıla Kaçmaz <sila.kacmaz@obase.com>
2022-11-12 11:44:34 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Login Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController txtUserName = TextEditingController();
+  TextEditingController txtPassword = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              child: const Text(
+                'Kullanıcı Giriş',
+                style: TextStyle(
+                    color: Colors.orange,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: TextField(
+                controller: txtUserName,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: TextField(
+                obscureText: true,
+                controller: txtPassword,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: ElevatedButton(
+                child: Text('Login'),
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              child: Checkbox(
+                activeColor: Colors.green,
+                onChanged: (newValue) {
+                  setState(() {});
+                  const Text("Remember me");
+                },
+                value: false,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
